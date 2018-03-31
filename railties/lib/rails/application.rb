0839009a45248473ed7b78b547f2666d80a99c2c
Yoshiyuki Kinjo <yskkin@gmail.com>
2018-03-31 21:47:04 +0900

    Deriving `secret_key_name` breaks key_generator defined in 5.1.
    If one created Rails 5.1 app and then updated to 5.2,
    `secret_key_base` defined in `config/secrets.yml` is ignored for
    `development` and `test` environment.
    A change in `secret_key_base` in turn breaks
    `Rails.application.key_generator`.
    
    If one encrypt data in Rails 5.1, she cannot decrypt it in Rails 5.2
    for `developemnt` and `test` environment.

--- railties/lib/rails/application.rb
+++ railties/lib/rails/application.rb
@@ -426,7 +426,7 @@ module Rails
     # then credentials.secret_key_base, and finally secrets.secret_key_base. For most applications,
     # the correct place to store it is in the encrypted credentials file.
     def secret_key_base
-      if Rails.env.test? || Rails.env.development?
+      if (Rails.env.test? || Rails.env.development?) && !secrets.secret_key_base
         Digest::MD5.hexdigest self.class.name
       else
         validate_secret_key_base(
